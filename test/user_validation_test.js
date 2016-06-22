7f152eba380f107cf694ce558dbcd682f3470832
localadmin <localadmin@compozed-tss-15.mobile.asu.edu>
2016-06-22 13:03:50 -0700

    finish user validation

--- test/user_validation_test.js
+++ test/user_validation_test.js
@@ -5,7 +5,7 @@ var User = require('../user')
 
 describe('User', function() {
   it('user can only be created with first name, last name, and valid email', function() {
-    var userParams = {firstName: "Tyler", lastName: "Komoroske, ", email: 'tjkomor@clemson.edu'}
+    var userParams = {firstName: "Tyler", lastName: "Komoroske", email: 'tjkomor@clemson.edu'}
 
     var user = new User(userParams);
 
@@ -20,20 +20,27 @@ describe('User', function() {
     expect(user.userValidator()).to.be.false
   });
 
-  it('user will not be valid without valid email address', function (){
-    var userParams = {firstName: "Tyler", lastName: "Komoroske", email: 'yolo'}
+  it('user will not be valid without valid email address and returns error', function (){
+    var userParams = {firstName: "Tyler", lastName: "Komoroske", email: 'yolo@'}
 
     var user = new User(userParams);
-
     expect(user.userValidator()).to.be.false
+    expect(user.errorMessage).to.equal("Must enter a valid email address")
+
+    var userParams2 = {firstName: "Tyler", lastName: "Komoroske", email: 'yolo@something.edu'}
+
+    var user2 = new User(userParams2);
+    expect(user2.userValidator()).to.be.true
   });
 
-  it('user will not be valid without valid first name', function (){
-    var userParams = {lastName: "Komoroske", email: 'yolo@something.com'}
+  it('user will not be valid without valid first name and returns error', function (){
+    var userParams = {lastName: "Komoroske", email: 'yolo@somet hing.com'}
 
     var user = new User(userParams);
 
     expect(user.userValidator()).to.be.false
+    expect(user.errorMessage).to.equal("Must enter valid alpahbetic charcaters")
+
   });
 
   it('user will not be valid without last name', function (){
@@ -43,4 +50,18 @@ describe('User', function() {
 
     expect(user.userValidator()).to.be.false
   });
+
+  it('user will not be valid if any non-alpahbetic chars are enetered', function (){
+    var userParams = {firstName: "  B  $%2", lastName: "Komoroske" ,email: 'yolo@something.com'}
+
+    var user = new User(userParams);
+
+    expect(user.userValidator()).to.be.false
+
+    var userParams = {firstName: "Tyler", lastName: "Komo5$roske" ,email: 'yolo@something.com'}
+    var user = new User(userParams);
+
+    expect(user.userValidator()).to.be.false
+  });
+
 });
--- user.js
+++ user.js
@@ -2,18 +2,30 @@
 
 function User(params) {
   this.params = params;
+  this.errorMessage = null
 }
 
 User.prototype.userValidator = function() {
-  if(this.params.firstName && this.params.lastName && this.emailValidator()) {
-    if(this.emailValidator()) return true
-    return false;
+  if(this.nameValidator(this.params.firstName) && this.nameValidator(this.params.lastName) && this.emailValidator(this.params.email)) {
+    return true;
   }
   return false;
 }
 
-User.prototype.emailValidator = function (){
-  if (this.params.email && this.params.email.includes('@')) return true
+User.prototype.nameValidator = function(name) {
+  var regex = /^[a-zA-Z ]{2,30}$/
+  if(name && regex.test(name)){
+    return true
+  }
+  this.errorMessage = "Must enter valid alpahbetic charcaters"
+  return false;
+}
+
+User.prototype.emailValidator = function (email){
+  if (email && email.includes('@') && email.includes('.') && !email.includes(' ')){
+    return true
+  }
+  this.errorMessage = "Must enter a valid email address"
 }
 
 module.exports = User;
